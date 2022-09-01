fe7ad7f3cab5b444ab09f4ad90d283a50d25e941
papatriz <papatriz@gmail.com>
2022-09-01 18:20:27 +0300

    Initial commit

---
+++ src/main/java/com/papatriz/jsfdemo/controllers/WelcomeController.java
@@ -0,0 +1,28 @@
+package com.papatriz.jsfdemo.controllers;
+
+import lombok.Data;
+import org.ocpsoft.rewrite.annotation.Join;
+        import org.ocpsoft.rewrite.el.ELBeanName;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.context.annotation.Scope;
+        import org.springframework.stereotype.Component;
+
+@Data
+@Scope(value = "session")
+@Component(value = "welcomeController")
+@ELBeanName(value = "welcomeController")
+
+@Join(path = "/", to = "/index.xhtml")
+public class WelcomeController {
+
+    private String message = "I am dynamic message";
+
+    public void setNewMessage(){
+       // setMessage(message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+        System.out.println("Message setted");
+    }
+}
---
+++ src/test/java/com/papatriz/jsfdemo/JsfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papatriz.jsfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
